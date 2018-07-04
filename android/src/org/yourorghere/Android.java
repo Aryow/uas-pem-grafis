052e92681ee0b8ca29ec087d1b6c0c602f1b95d7
Aryow <aryow93@gmail.com>
2018-07-04 14:50:59 +0700

    Aryo

--- android/src/org/yourorghere/Android.java
+++ android/src/org/yourorghere/Android.java
@@ -2,39 +2,80 @@ package org.yourorghere;
 
 import com.sun.opengl.util.Animator;
 import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.EventQueue;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import javax.media.opengl.GL;
-import javax.media.opengl.GLAutoDrawable;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.media.opengl.GLCanvas;
-import javax.media.opengl.GLEventListener;
-import javax.media.opengl.glu.GLU;
+import javax.media.opengl.GLCapabilities;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPopupMenu;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
 
-/**
- * Android.java <BR>
- * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel)
- * <P>
- *
- * This version is equal to Brian Paul's version 1.2 1999/10/21
- */
-public class Android implements GLEventListener {
-    float angle = 0;
+public class Android extends JFrame {
 
-    public static void main(String[] args) {
-        Frame frame = new Frame("Tugas 4 - Logo Android");
-        GLCanvas canvas = new GLCanvas();
+    static {
+// When using a GLCanvas, we have to set the Popup-Menues to be HeavyWeight,
+// so they display properly on top of the GLCanvas
+        JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+    }
+    private Animator animator;
 
-        canvas.addGLEventListener(new Android());
-        frame.add(canvas);
-        frame.setSize(640, 480);
-        final Animator animator = new Animator(canvas);
-        frame.addWindowListener(new WindowAdapter() {
+    /**
+     * Creates new form MainFrame
+     */
+    public Android() {
+        initComponents();
+        final GLRenderer glrender = new GLRenderer();
+        KeyListener mylisterner = new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+                System.out.println("typed " + e.getKeyCode());
+            }
+
+            public void keyPressed(KeyEvent e) {
+                System.out.println("pressed " + e.getKeyCode());
+                glrender.Key_Pressed(e.getKeyCode());
+                canvas.repaint();
+            }
+
+            public void keyReleased(KeyEvent e) {
+                System.out.println("released " + e.getKeyCode());
+            }
+        };
+        canvas.addGLEventListener(glrender);
+        Timer mytimer = new Timer();
+        TimerTask mytimertask = new TimerTask() {
+            @Override
+            public void run() {
+//glrender.draw_text(glut, gl);
+                canvas.repaint();
+//To change body of generated methods, choose Tools | Templates.
+            }
+        };
+        mytimer.schedule(mytimertask, 100, 100);
+        addKeyListener(mylisterner);
+        animator = new Animator(canvas);
+        this.setLocationRelativeTo(null);
+        this.setSize(600, 400);
+        animator = new Animator(canvas);
+// This is a workaround for the GLCanvas not adjusting its size, when the frame is resized.
+        canvas.setMinimumSize(new Dimension());
+        this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
+// Run this on another thread than the AWT event queue to
+// make sure the call to Animator.stop() completes before
+// exiting
                 new Thread(new Runnable() {
                     public void run() {
                         animator.stop();
@@ -43,215 +84,89 @@ public class Android implements GLEventListener {
                 }).start();
             }
         });
-// Center frame
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        animator.start();
-    }
-    private float view_rotx = 20.0f;
-    private float view_roty = 30.0f;
-    private int oldMouseX;
-    private int oldMouseY;
-
-    public void init(GLAutoDrawable drawable) {
-        GL gl = drawable.getGL();
-        gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-        gl.glShadeModel(GL.GL_FLAT);
-        float ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
-        float difusse[] = {1.0f, 1.0f, 1.0f, 1.0f};
-        float specular[] = {0.2f, 1.0f, 0.2f, 1.0f};
-        float position[] = {20.0f, 30.0f, 20.0f, 0.0f};
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambient, 0);
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, difusse, 0);
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, position, 0);
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, specular, 0);
-        float[] mambient = {0.2f, 1.01175f, 0.2f, 0f
-        };
-        float[] mdiffuse = {0.61424f, 0.04136f, 0.04136f, 0.55f};
-        float[] mspecular = {0.727811f, 0.626959f, 0.626959f, 0.55f};
-        float mshine = 76.8f;
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT, mambient, 0);
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_DIFFUSE, mdiffuse, 0);
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, mspecular, 0);
-        gl.glMaterialf(GL.GL_FRONT, GL.GL_SHININESS, mshine);
-        gl.glEnable(GL.GL_LIGHTING);
-        gl.glEnable(GL.GL_LIGHT0);
-        gl.glEnable(GL.GL_DEPTH_TEST);
-        gl.glEnable(GL.GL_NORMALIZE);
-//        drawable.addMouseListener(this);
-//        drawable.addMouseMotionListener(this);
     }
 
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        GL gl = drawable.getGL();
-        GLU glu = new GLU();
-        if (height <= 0) {
-            height = 1;
+    @Override
+    public void setVisible(boolean show) {
+        if (!show) {
+            animator.stop();
         }
-        final float h = (float) width / (float) height;
-        gl.glViewport(0, 0, width, height);
-        gl.glMatrixMode(GL.GL_PROJECTION);
-        gl.glLoadIdentity();
-        glu.gluPerspective(45.0f, h, 1.0, 20.0);
-        gl.glMatrixMode(GL.GL_MODELVIEW);
-        gl.glLoadIdentity();
+        super.setVisible(show);
+        if (!show) {
+            animator.start();
+        }
     }
 
-    public void display(GLAutoDrawable drawable) {
-        angle += 10;
-        GL gl = drawable.getGL();
-        GLU glu = new GLU();
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-        gl.glLoadIdentity();
-        glu.gluLookAt(4, 4, 4, // eye pos
-                0, 0, 0, // look at
-                0, 0, 1); // up
-        gl.glTranslatef(0f, 0f, 0f);
-//        gl.glRotatef(angle, 1.0f, 0.0f, 0.0f);
-//        
-        //gl.glTranslatef(-0.5f, 2.5f, -1.5f);
-// Objek kepala
-        gl.glTranslatef(-5.5f, -5.5f, -1.5f);
-         gl.glRotatef(180, 0.0f, 10.0f, 0.0f);
-        gl.glPushMatrix();
-        
-        Objek.Setengah(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-        gl.glPopMatrix();
-// Objek Bola
-        gl.glTranslatef(1.0f, 1.0f, 1.0f);
-        gl.glPushMatrix();
-        Objek.Tabung3(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+// <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        JLabel label = new JLabel();
+        canvas = new GLCanvas(createGLCapabilites());
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        label.setText("Gunakan Keyboard untuk menggerakkan kamera");
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(Alignment.LEADING)
+                                .addComponent(canvas, GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
+                                .addComponent(label))
+                        .addContainerGap())
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(label)
+                        .addPreferredGap(ComponentPlacement.RELATED)
+                        .addComponent(canvas, GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
+                        .addContainerGap())
+        );
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
 
-        gl.glPopMatrix();
-
-        gl.glTranslatef(-1f, -2.5f, 0f);
-        gl.glPushMatrix();
-        Objek.Tabung3(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-
-        gl.glPopMatrix();
-        gl.glFlush();
-
-        gl.glTranslatef(0f, 0f, 0f);
-        gl.glPushMatrix();
-        Objek.Bola2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-
-        gl.glPopMatrix();
-
-        gl.glTranslatef(0f, 0f, 2.5f);
-//gl.glPushMatrix();
-//Bola.Bola2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-//
-////gl.glPopMatrix();
-//
-//        gl.glTranslatef(1f, 2.5f, 0f);
-////gl.glPushMatrix();
-////Bola.Bola2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-////
-////gl.glPopMatrix();
-//
-//        gl.glTranslatef(0f, 3.5f, -2.5f);
-//        gl.glPushMatrix();
-//        Objek.Bola2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-//
-//        gl.glPopMatrix();
-//        gl.glFlush();
-
-        gl.glTranslatef(0f, 1.5f, -3f);
-         
-        gl.glPushMatrix();
-        Objek.Tabung1(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-
-        gl.glPopMatrix();
-        gl.glFlush();
-
-        gl.glTranslatef(0f, 0.5f, 1f);
-        gl.glPushMatrix();
-        Objek.Tabung2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-
-        gl.glPopMatrix();
-        gl.glFlush();
-
-        gl.glTranslatef(0f, 0f, 2.6f);
-        gl.glPushMatrix();
-        Objek.Bola2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-
-        gl.glPopMatrix();
-        gl.glFlush();
-
-        gl.glTranslatef(0f, -1f, -2.7f);
-        gl.glPushMatrix();
-        Objek.Tabung2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-
-        gl.glPopMatrix();
-        gl.glFlush();
-
-        gl.glTranslatef(0f, 0f, 2.5f);
-        gl.glPushMatrix();
-        Objek.Bola2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-
-        gl.glPopMatrix();
-        gl.glFlush();
-//
-//        gl.glTranslatef(0f, -0.9f, -1.4f);
-//        gl.glPushMatrix();
-//        Objek.Bola2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-//
-//        gl.glPopMatrix();
-//        gl.glFlush();
-//
-//        gl.glTranslatef(1f, -0.5f, 0f);
-//        gl.glPushMatrix();
-//        Objek.Bola2(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-//
-//        gl.glPopMatrix();
-//        gl.glFlush();
-//
-//        gl.glTranslatef(-0.2f, -0.5f, -0.8f);
-//        gl.glPushMatrix();
-//        Objek.Tabung4(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-//
-//        gl.glPopMatrix();
-//        gl.glFlush();
-//
-//        gl.glTranslatef(-0.6f, -1.7f, 0f);
-//        gl.glPushMatrix();
-//        Objek.Tabung4(gl); // Pada fungsi yang sudah dibuat pada pembahasan sebelumnya
-//
-//        gl.glPopMatrix();
-//        gl.glFlush();
-
+    /**
+     * Called from within initComponents(). hint: to customize the generated
+     * code choose 'Customize Code' in the contextmenu of the selected UI
+     * Component you wish to cutomize in design mode.
+     *
+     * @return Returns customized GLCapabilities.
+     */
+    private GLCapabilities createGLCapabilites() {
+        GLCapabilities capabilities = new GLCapabilities();
+        capabilities.setHardwareAccelerated(true);
+// try to enable 2x anti aliasing - should be supported on most hardware
+        capabilities.setNumSamples(2);
+        capabilities.setSampleBuffers(true);
+        return capabilities;
     }
 
-    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+// Run this in the AWT event thread to prevent deadlocks and race conditions
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+// switch to system l&f for native font rendering etc.
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception ex) {
+                    Logger.getLogger(getClass().getName()).log(Level.INFO, "can not enable system look and feel", ex);
+                }
+                Android frame = new Android();
+                frame.setVisible(true);
+            }
+        });
     }
-
-    public void mouseClicked(MouseEvent e) {
-    }
-
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    public void mouseExited(MouseEvent e) {
-    }
-
-    public void mouseReleased(MouseEvent e) {
-    }
-
-    public void mousePressed(MouseEvent e) {
-        oldMouseX = e.getX();
-        oldMouseY = e.getY();
-    }
-
-//    public void mouseDragged(MouseEvent e) {
-//        int x = e.getX();
-//        int y = e.getY();
-//        Dimension size = e.getComponent().getSize();
-//        float thetaY = 360.0f * ((float) (x - oldMouseX) / (float) size.width);
-//        float thetaX = 360.0f * ((float) (oldMouseY - y) / (float) size.height);
-//        oldMouseX = x;
-//        oldMouseY = y;
-//        view_rotx += thetaX;
-//        view_roty += thetaY;
+// Variables declaration - do not modify//GEN-BEGIN:variables
+    private GLCanvas canvas;
+// End of variables declaration//GEN-END:variables
 }
-
-    
